f2436bdd9c88e6a06e11c32d03e6533ef046c8de
Massih Chopan <194472108+FulSt-Mas@users.noreply.github.com>
2025-03-12 15:55:43 +0100

    dynamic Seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :user_skills
+  has_many :skills, through: :user_skills, source: :skill
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
