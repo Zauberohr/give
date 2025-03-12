66b14120a8f700b7fc7cc53eb41be331bff2f551
otto solms <ottosolms@me.com>
2025-03-12 16:51:59 +0100

    chat thing is done i think

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,29 @@
+class RequestsController < ApplicationController
+  before_action :set_user_skill, only: [:create]
+
+  def show
+    @request = Request.find(params[:id])
+    @message = Message.new
+  end
+
+  def create
+    @request = Request.new(request_params)
+    @request.user_skill = @user_skill
+    @request.user = current_user
+    if @request.save
+      redirect_to @request, notice: "Request erfolgreich erstellt."
+    else
+      render "user_skills/show"
+    end
+  end
+
+  private
+
+  def set_user_skill
+    @user_skill = UserSkill.find(params[:user_skill_id])
+  end
+
+  def request_params
+    params.require(:request).permit(:date)
+  end
+end
