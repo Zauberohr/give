5e02dbea7591a286b2442015b20f22c09e7745d6
Naakul Vashist <nakul1851@gmail.com>
2025-03-13 15:36:11 +0100

    home page update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,9 @@
-# class PagesController < ApplicationController
-#   skip_before_action :authenticate_user!, only: [ :home ]
-
-#   def home
-#   end
-# end
-
-
-
 class PagesController < ApplicationController
   # Disable authentication check for the home action
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @user_skills = UserSkill.all.uniq { |user_skill| user_skill.skill_id }.uniq { |user_skill| user_skill.user_id }.sample(4)
   end
 
 end
