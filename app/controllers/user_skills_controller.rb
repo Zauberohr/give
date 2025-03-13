6e820e2236b0489c44f1305b8fa38d7ace07b76a
Massih Chopan <194472108+FulSt-Mas@users.noreply.github.com>
2025-03-13 15:36:11 +0100

    user_skills

--- app/controllers/user_skills_controller.rb
+++ app/controllers/user_skills_controller.rb
@@ -8,6 +8,20 @@ class UserSkillsController < ApplicationController
   end
 
   def show
+    # Find the user_skill instance based on the ID passed
     @user_skill = UserSkill.find(params[:id])
+
+    # Retrieve the user for this user_skill
+    @user = @user_skill.user
+
+    # Get all the skills for the user, ordered in a way that the searched skill comes first (if a search exists)
+    all_user_skills = @user.user_skills.where.not(id: params[:id]).includes(:skill)
+    search_skill = params[:query].present? ? Skill.find_by(name: params[:query].capitalize) : nil
+
+    if search_skill
+      @user_skills_sorted = all_user_skills.sort_by { |us| us.skill == search_skill ? 0 : 1 }
+    else
+      @user_skills_sorted = all_user_skills
+    end
   end
 end
