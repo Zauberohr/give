66e4fece65fdce9ee7107d83acbf2a5c71fdcc43
otto solms <ottosolms@me.com>
2025-03-13 15:26:39 +0100

    changed some stuff for the navbar and chat works

--- app/controllers/user_skills_controller.rb
+++ app/controllers/user_skills_controller.rb
@@ -1,7 +1,11 @@
 class UserSkillsController < ApplicationController
 
   def index
+
     @users = User.all
     # @user_skills = UserSkill.all
+
+    # / thats for the logo
+    @role = :giver
   end
 end
