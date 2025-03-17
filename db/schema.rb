4c8e4b47e280339d71be21511db2b3523ed6d8c6
Massih Chopan <194472108+FulSt-Mas@users.noreply.github.com>
2025-03-17 11:17:58 +0100

    Seed_skills_show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_13_142048) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_17_091532) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_13_142048) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "user_skills", force: :cascade do |t|
