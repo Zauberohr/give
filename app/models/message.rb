087456d2e1f65bbf0d23d8c8388b609d064093c5
Naakul Vashist <nakul1851@gmail.com>
2025-03-17 12:13:21 +0100

    mssages working

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,14 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :request
+  after_create_commit :broadcast_message
+
+  private
+
+  def broadcast_message
+    broadcast_append_to "request_#{request.id}_messages",
+                        partial: "messages/message",
+                        target: "messages",
+                        locals: { message: self, user: user}
+  end
 end
