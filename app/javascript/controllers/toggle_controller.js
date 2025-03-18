39e064c1e2a2b768bb2c076eea6cb77c795495df
otto solms <ottosolms@me.com>
2025-03-18 16:34:27 +0100

    messages are done

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -3,16 +3,34 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="toggle"
 export default class extends Controller {
 
-  static targets = ["togglableElement"]
+  static targets = ["sent", "received", "sentButton", "receivedButton"]
 
-  fire(event) {
-    this.togglableElementTargets.forEach((request) => {
-      request.classList.toggle("d-none");
-    })
-    if (event.currentTarget.innerText === 'Requests Recieved'){
-      event.currentTarget.innerText = 'Your Requests'
-    } else {
-      event.currentTarget.innerText = 'Requests Recieved'
-    }
+  fireSent(event) {
+    console.log(this.sentTarget);
+
+    // Toggle the requests
+    this.sentTarget.classList.remove("d-none");
+    this.receivedTarget.classList.add("d-none");
+
+    // Make sent button primary
+    this.sentButtonTarget.classList.add("btn-primary");
+    this.sentButtonTarget.classList.remove("btn-outline-primary");
+
+    // Make received button outline
+    this.receivedButtonTarget.classList.remove("btn-primary");
+    this.receivedButtonTarget.classList.add("btn-outline-primary");
+  }
+
+  fireReceived(event) {
+    console.log(this.receivedTarget);
+
+    this.sentTarget.classList.add("d-none");
+    this.receivedTarget.classList.remove("d-none");
+
+    this.sentButtonTarget.classList.remove("btn-primary");
+    this.sentButtonTarget.classList.add("btn-outline-primary");
+
+    this.receivedButtonTarget.classList.add("btn-primary");
+    this.receivedButtonTarget.classList.remove("btn-outline-primary");
   }
 }
